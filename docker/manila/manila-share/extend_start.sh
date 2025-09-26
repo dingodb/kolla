059cead3cb27dacea86a7fe46fcc57dcb882fb5c
hanzhuang <hanzhuang@zetyun.com>
2025-09-26 18:08:37 +0800

    [manila] add clone from new repo

--- docker/manila/manila-share/extend_start.sh
+++ docker/manila/manila-share/extend_start.sh
@@ -1,1 +1,1 @@
-/usr/bin/ganesha.nfsd -F -L STDERR -N NIV_EVENT > /var/log/kolla/manila/ganesha.log 2>&1 &
+sudo /usr/bin/ganesha.nfsd -F -L STDERR -N NIV_EVENT > /var/log/kolla/manila/ganesha.log 2>&1 &
